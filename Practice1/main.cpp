b75e5b4a0a4113678ded43e7d998923cdd063ca6
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2023-01-16 20:31:23 -0500

    first test c++

---
+++ Practice1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int number;
+
+    cout << "Testing VSC" << endl << endl;
+    cout << "Enter a number:";
+    cin >> number;
+    cout << "You entered " << number << endl;
+
+
+    return 0;
+}
