0b15b31b323d5d4c17690443f8ddf69124601180
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2024-05-25 18:35:02 -0300

    created gauchao.cpp

---
+++ Gauchao/gauchao.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file gauchao.cpp
+ * @author Lucas Hebmuller (lucas.hebmuller@gmail.com)
+ * @brief This file implements gauchao.cpp, a project that gives an alternative championship model for gauchao
+ * @version 0.1
+ * @date 2024-05-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+#include <iostream>
+
+
+/*
+    Teams and their divisions(2024):
+    Avenida             D
+    Brasil de Pelotas   D
+    Gremio              A
+    Guarany             D
+    Internacional       A
+    Juventude           A
+    Novo Hamburgo       D
+    Santa Cruz          -
+    Sao Jose            C
+    Sao Luiz            D
+    Ypiranga            C
+*/
+
+
+int main()
+{
+
+}
