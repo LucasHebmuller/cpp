ae9ef6db3252e2d7d80656b1691d6c49d3bd2810
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2024-05-31 14:18:00 -0300

    using assert

---
+++ TestingAssert/assert.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cassert> // library to be tested
+using namespace std;
+
+int main()
+{
+    int a = 15, b = 10;
+
+    assert(b != 10);
+    cout << a;
+
+    return 0;
+}
